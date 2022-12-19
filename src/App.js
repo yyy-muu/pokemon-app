6ac682c805f5aee9c2bb0ba41ad1fc6f00a00690
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-12-19 14:38:21 +0900

    Add getting all pokemons data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+
+import { getAllPokemon } from "./utils/pokemon";
 import "./App.css";
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //全ポケモンデータを取得する
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
   return <div className="App"></div>;
 }
 

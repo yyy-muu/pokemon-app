295e4933f1208be100cd42a3cb67b5bf0dc939f3
yyy-muu <rirakkuma.0918.y@gmail.com>
2022-12-19 20:34:19 +0900

    Add & fix card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import { Card } from "./components/Card/Card";
 import "./App.css";
 
 function App() {
@@ -40,7 +41,11 @@ function App() {
         <h1>ロード中</h1>
       ) : (
         <>
-          <h1>ポケモンデータを取得しました</h1>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
         </>
       )}
     </div>
